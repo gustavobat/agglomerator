e6c624f2558ae361f7b98009693dbab39207f934
Gustavo Batistela <gustavoabatistela@gmail.com>
2020-09-23 19:05:36 -0300

    Included initial PZ project file structure

---
+++ Tools.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Gustavo A. Batistela on 22/08/2020.
+//
+
+#include "Tools.h"
---
+++ Tools.h
@@ -0,0 +1,10 @@
+//
+// Created by Gustavo A. Batistela on 22/08/2020.
+//
+
+#ifndef AGGLOMERATOR_TOOLS_H
+#define AGGLOMERATOR_TOOLS_H
+
+
+
+#endif //AGGLOMERATOR_TOOLS_H
